4f0b46ea9fcd1416116ba75b536aa2671cb3b8ab
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-06-17 14:29:08 +0545

    cli

--- main.go
+++ main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 )
-
-func scan(path string) {
-	fmt.Printf("Found folders: \n\n")
-	repositories := recursiveScanFolder(path)
-	filePath := getDotFilePath()
-	addNewSliceElementsToFile(filePath, repositories)
-	fmt.Printf("\n\nSuccessfully added\n\n")
-}
-
-func stats(email string) {
-	print(email)
-}
 
 func main() {
 	var folder string
 	var email string
-	flag.StringVar(&folder, "add", "", "add a new folder to scan for git repos.")
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
 	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
 	flag.Parse()
 
@@ -31,3 +18,4 @@ func main() {
 
 	stats(email)
 }
+
--- scan.go
+++ scan.go
@@ -4,11 +4,102 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
 	"strings"
 )
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, 0755)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// file does not exist
+			_, err = os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			// other error
+			panic(err)
+		}
+	}
+
+	return f
+}
+
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+
+	return lines
+}
+
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+// joinSlices adds the element of the `new` slice
+// into the `existing` slice, only if not already there
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+	return existing
+}
+
+func dumpStringsSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringsSliceToFile(repos, filePath)
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+func scan(folder string) {
+	fmt.Printf("Found folders:\n\n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
 
 func scanGitFolders(folders []string, folder string) []string {
 	// trim the last `/`
@@ -18,7 +109,6 @@ func scanGitFolders(folders []string, folder string) []string {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	files, err := f.Readdir(-1)
 	f.Close()
 	if err != nil {
@@ -30,93 +120,19 @@ func scanGitFolders(folders []string, folder string) []string {
 	for _, file := range files {
 		if file.IsDir() {
 			path = folder + "/" + file.Name()
-			fmt.Println(path)
-			folders = append(folders, path)
-			continue
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
 		}
-		if file.Name() == "vendor" || file.Name() == "node_modules" {
-			continue
-		}
-		folders = scanGitFolders(folders, path)
 	}
+
 	return folders
 }
 
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func getDotFilePath() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return usr.HomeDir + "/.gitlocalstats"
-}
-
-func addNewSliceElementsToFile(filePath string, newRepos []string) {
-	existingRepos := parseFileLinesToSlice(filePath)
-	repos := joinSlices(newRepos, existingRepos)
-	dumpStringsSliceToFile(repos, filePath)
-}
-
-func parseFileLinesToSlice(filePath string) []string {
-	f := openFile(filePath)
-	defer f.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(f)
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		if err != io.EOF {
-			panic(err)
-		}
-	}
-
-	return lines
-}
-
-func openFile(filePath string) *os.File {
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0755)
-	if err != nil {
-		if os.IsNotExist(err) {
-			_, err = os.Create(filePath)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			panic(err)
-		}
-	}
-
-	return f
-}
-
-func joinSlices(new []string, existing []string) []string {
-	for _, i := range new {
-		if !sliceContains(existing, i) {
-			existing = append(existing, i)
-		}
-	}
-
-	return existing
-}
-
-func sliceContains(slice []string, value string) bool {
-	for _, v := range slice {
-		if v == value {
-			return true
-		}
-	}
-
-	return false
-}
-
-func dumpStringsSliceToFile(repos []string, filePath string) {
-	content := strings.Join(repos, "\n")
-	os.WriteFile(filePath, []byte(content), 0755)
-}
