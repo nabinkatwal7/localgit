9e8ad82c9e7ee89dafa5046527912872c25034f4
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-06-17 12:40:33 +0545

    scan files

--- main.go
+++ main.go
@@ -3,55 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"os"
-	"os/user"
-	"strings"
 )
-
-func scanGitFolders(folders []string, folder string) []string {
-	// trim the last `/`
-	folder = strings.TrimSuffix(folder, "/")
-
-	f, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	files, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var path string
-
-	for _, file := range files {
-		if file.IsDir() {
-			path = folder + "/" + file.Name()
-			fmt.Println(path)
-			folders = append(folders, path)
-			continue
-		}
-		if file.Name() == "vendor" || file.Name() == "node_modules" {
-			continue
-		}
-		folders = scanGitFolders(folders, path)
-	}
-	return folders
-}
-
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func getDotFilePath() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return usr.HomeDir + "/.gitlocalstats"
-}
 
 func scan(path string) {
 	fmt.Printf("Found folders: \n\n")
